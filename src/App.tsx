9e41fb7e54bf2bafca4fcdbedf2aaac0f27f9a5d
DangDevTH <dang.dev.th@gmail.com>
2025-07-31 11:22:08 +0700

    test
    test

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import liff from "@line/liff";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [profile, setProfile] = useState<any>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [idToken, setIdToken] = useState<string | null>(null);
+  const loginLine = async () => {
+    try {
+      await liff.init({ liffId: "2007844970-wd1e003k" });
+      liff.login({ redirectUri: window.location.href });
+      const userProfile = await liff.getProfile();
+      const token = liff.getIDToken();
+      console.log("userProfile", liff.getContext());
+      console.log("token", token);
+      setProfile(userProfile);
+      setIdToken(token);
+      setIsLoggedIn(true);
+    } catch (err) {
+      console.error("LIFF init error:", err);
+    }
+  };
+
+  const getContext = async () => {
+    try {
+      // await liff.init({ liffId: "2007844970-wd1e003k" });
+      window.open("https://page.line.me/536whbrf", "_blank");
+      // console.log("context:", liff.getContext());
+      // console.log("os:", liff.getOS());
+    } catch (err) {
+      console.error("LIFF init error:", err);
+    }
+  };
+
+  console.log("Token", idToken);
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {isLoggedIn ? (
+        <div>
+          <h2>Welcome, {profile?.displayName}</h2>
+          <img src={profile?.pictureUrl} alt="profile" style={{ width: 100 }} />
+          <p>User ID: {profile?.userId}</p>
+          <button onClick={() => liff.logout()}>ผูกบัญชีไลน์</button>
+        </div>
+      ) : (
+        <div>
+          Login line <button onClick={() => getContext()}>ผูกบัญชีไลน์</button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
